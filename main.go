7fd4ebb7f26a36f92658d22898317c2a38eca119
Jack Conner <1jackconner@gmail.com>
2024-05-28 18:09:50 -0700

    implement fileserver

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
+const port string = "8080"
+const filepathRoot string = "/"
+
 func main() {
 	mux := http.NewServeMux()
-	srv := http.Server{
-		Addr:    "localhost:8080",
+
+	srv := &http.Server{
+		Addr:    ":" + port,
 		Handler: mux,
 	}
-	srv.ListenAndServe()
+	mux.Handle(filepathRoot, http.FileServer(http.Dir(".")))
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+
 }
