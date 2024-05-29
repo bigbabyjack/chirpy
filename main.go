13d96e96bb3dfa3f9f608140f084cd64191d0829
Jack Conner <1jackconner@gmail.com>
2024-05-28 20:46:40 -0700

    add middleware

--- main.go
+++ main.go
@@ -1,21 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-const port string = "8080"
-const filepathRoot string = "/"
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Print("Increasing count.\n")
+		cfg.fileserverHits++
+		log.Printf("Count of hits: %v", cfg.fileserverHits)
+		next.ServeHTTP(w, r) // call the next handler
+	})
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
 
 func main() {
-	mux := http.NewServeMux()
+	cfg := &apiConfig{
+		fileserverHits: 0,
+	}
+	const port string = "8080"
+	const filepathRoot string = "/"
 
+	mux := http.NewServeMux()
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+
+	mux.Handle("/app/*", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits)))
+	})
+	mux.HandleFunc("/reset", cfg.handlerReset)
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
